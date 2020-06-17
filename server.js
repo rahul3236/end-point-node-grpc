2ea6b094bde590d2bec86d950765f513d7dd06ec
Vlad Khazin <vladimmir.khazin@icssolutions.ca>
2020-06-17 14:34:11 -0400

    adding message to log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const PROTO_PATH = "./helloworld.proto";
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
+const PORT = process.env.GRPC_SERVER_PORT || 50051;
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -19,12 +20,11 @@ function main() {
   var server = new grpc.Server();
   server.addService(hello_proto.Hello.service, { sayHello: sayHello });
   server.bind(
-    `${process.env.GRPC_HOST || "0.0.0.0"}:${
-      process.env.GRPC_SERVER_PORT || 50051
-    }`,
+    `${process.env.GRPC_HOST || "0.0.0.0"}:${PORT}`,
     grpc.ServerCredentials.createInsecure()
   );
   server.start();
+  console.info(`started service on port: ${PORT}`);
 }
 
 main();
